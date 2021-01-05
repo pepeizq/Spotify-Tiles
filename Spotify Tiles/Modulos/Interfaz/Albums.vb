77597aad1044228d4359918cdbe36ac3456a8093
Jose Luis Izquierdo <pepeizq@msn.com>
2021-01-05 08:28:55 +0100

    Agregar archivos de proyecto.

---
+++ Spotify Tiles/Modulos/Interfaz/Albums.vb
@@ -0,0 +1,130 @@
+﻿Imports Microsoft.Toolkit.Uwp.UI.Controls
+Imports SpotifyAPI.Web
+Imports Windows.UI
+
+Namespace Interfaz
+
+    Module Albums
+
+        Public anchoColumna As Integer = 250
+
+        Public Sub Cargar()
+
+            Dim recursos As New Resources.ResourceLoader
+
+            Dim frame As Frame = Window.Current.Content
+            Dim pagina As Page = frame.Content
+
+            Dim tbBuscadorAlbums As TextBox = pagina.FindName("tbBuscadorAlbums")
+
+            AddHandler tbBuscadorAlbums.TextChanged, AddressOf BuscadorAlbumsTextoCambia
+
+            Dim botonBuscarAlbums As Button = pagina.FindName("botonBuscarAlbums")
+            botonBuscarAlbums.IsEnabled = False
+
+            AddHandler botonBuscarAlbums.Click, AddressOf BuscarAlbumsClick
+            AddHandler botonBuscarAlbums.PointerEntered, AddressOf EfectosHover.Entra_Boton_IconoTexto
+            AddHandler botonBuscarAlbums.PointerExited, AddressOf EfectosHover.Sale_Boton_IconoTexto
+
+        End Sub
+
+        Private Sub BuscadorAlbumsTextoCambia(sender As Object, e As TextChangedEventArgs)
+
+            Dim frame As Frame = Window.Current.Content
+            Dim pagina As Page = frame.Content
+
+            Dim botonBuscarAlbums As Button = pagina.FindName("botonBuscarAlbums")
+
+            Dim tb As TextBox = sender
+
+            If tb.Text.Trim.Length > 2 Then
+                botonBuscarAlbums.IsEnabled = True
+            Else
+                botonBuscarAlbums.IsEnabled = False
+            End If
+
+        End Sub
+
+        Private Async Sub BuscarAlbumsClick(sender As Object, e As RoutedEventArgs)
+
+            Dim boton As Button = sender
+            boton.IsEnabled = False
+
+            Dim frame As Frame = Window.Current.Content
+            Dim pagina As Page = frame.Content
+
+            Dim pbBuscadorAlbums As ProgressBar = pagina.FindName("pbBuscadorAlbums")
+            pbBuscadorAlbums.Visibility = Visibility.Visible
+
+            Dim tbBuscadorAlbums As TextBox = pagina.FindName("tbBuscadorAlbums")
+            tbBuscadorAlbums.IsEnabled = False
+
+            Dim resultados As SearchResponse = Await Spotify.Buscador(tbBuscadorAlbums.Text.Trim)
+
+            If Not resultados Is Nothing Then
+                Dim gv As AdaptiveGridView = pagina.FindName("gvTiles")
+                gv.DesiredWidth = anchoColumna
+                gv.Items.Clear()
+
+                Dim listaAlbums As New List(Of Tile)
+
+                For Each album In resultados.Albums.Items
+                    Dim album2 As New Tile(album.Name, album.Id, album.Uri, album.Images(0).Url, album.Images(0).Url, album.Images(0).Url, album.Images(0).Url)
+                    listaAlbums.Add(album2)
+                Next
+
+                For Each album In listaAlbums
+                    Dim panel As New DropShadowPanel With {
+                        .Margin = New Thickness(10, 10, 10, 10),
+                        .ShadowOpacity = 0.9,
+                        .BlurRadius = 10,
+                        .MaxWidth = anchoColumna + 20,
+                        .HorizontalAlignment = HorizontalAlignment.Center,
+                        .VerticalAlignment = VerticalAlignment.Center
+                    }
+
+                    Dim botonAlbum As New Button
+
+                    Dim imagen As New ImageEx With {
+                        .Source = album.ImagenGrande,
+                        .IsCacheEnabled = True,
+                        .Stretch = Stretch.UniformToFill,
+                        .Padding = New Thickness(0, 0, 0, 0),
+                        .HorizontalAlignment = HorizontalAlignment.Center,
+                        .VerticalAlignment = VerticalAlignment.Center,
+                        .EnableLazyLoading = True
+                    }
+
+                    botonAlbum.Tag = album
+                    botonAlbum.Content = imagen
+                    botonAlbum.Padding = New Thickness(0, 0, 0, 0)
+                    botonAlbum.Background = New SolidColorBrush(Colors.Transparent)
+
+                    panel.Content = botonAlbum
+
+                    Dim tbToolTip As TextBlock = New TextBlock With {
+                        .Text = album.Titulo,
+                        .FontSize = 16,
+                        .TextWrapping = TextWrapping.Wrap
+                    }
+
+                    ToolTipService.SetToolTip(botonAlbum, tbToolTip)
+                    ToolTipService.SetPlacement(botonAlbum, PlacementMode.Mouse)
+
+                    AddHandler botonAlbum.Click, AddressOf Spotify.BotonTile_Click
+                    AddHandler botonAlbum.PointerEntered, AddressOf Entra_Boton_Imagen
+                    AddHandler botonAlbum.PointerExited, AddressOf Sale_Boton_Imagen
+
+                    gv.Items.Add(panel)
+                Next
+            End If
+
+            pbBuscadorAlbums.Visibility = Visibility.Collapsed
+            boton.IsEnabled = True
+            tbBuscadorAlbums.IsEnabled = True
+
+        End Sub
+
+    End Module
+
+End Namespace
---
+++ Spotify Tiles/Modulos/Spotify.vb
@@ -0,0 +1,121 @@
+﻿Imports Microsoft.Toolkit.Uwp.UI.Controls
+Imports SpotifyAPI.Web
+Imports Windows.UI.Xaml.Media.Animation
+
+Module Spotify
+
+    'https://developer.spotify.com/dashboard/applications/ec4978f924524f1cabe6394c2ad44329
+    'https://johnnycrazy.github.io/SpotifyAPI-NET/
+
+    Dim cliente As SpotifyClient
+
+    Public Sub Cargar()
+
+        Dim config As SpotifyClientConfig = SpotifyClientConfig.CreateDefault.WithAuthenticator(New ClientCredentialsAuthenticator("ec4978f924524f1cabe6394c2ad44329", "660fb9bee11048ecb7f2673e78d69e7b"))
+        cliente = New SpotifyClient(config)
+
+        Dim recursos As New Resources.ResourceLoader
+
+        Dim frame As Frame = Window.Current.Content
+        Dim pagina As Page = frame.Content
+
+        Dim gridAlbums As Grid = pagina.FindName("gridAlbums")
+        Interfaz.Pestañas.Visibilidad(gridAlbums, recursos.GetString("Albums"), Nothing)
+
+    End Sub
+
+    Public Async Function Buscador(texto As String) As Task(Of SearchResponse)
+
+        Dim peticion As New SearchRequest(SearchRequest.Types.Album, texto) With {
+            .Limit = 50
+        }
+
+        Dim resultados As SearchResponse = Await cliente.Search.Item(peticion)
+
+        Return resultados
+
+    End Function
+
+    Public Sub BotonTile_Click(sender As Object, e As RoutedEventArgs)
+
+        Trial.Detectar()
+        Interfaz.AñadirTile.ResetearValores()
+
+        Dim frame As Frame = Window.Current.Content
+        Dim pagina As Page = frame.Content
+
+        Dim botonJuego As Button = e.OriginalSource
+        Dim juego As Tile = botonJuego.Tag
+
+        Dim botonAñadirTile As Button = pagina.FindName("botonAñadirTile")
+        botonAñadirTile.Tag = juego
+
+        Dim imagenJuegoSeleccionado As ImageEx = pagina.FindName("imagenJuegoSeleccionado")
+        imagenJuegoSeleccionado.Source = juego.ImagenAncha
+
+        Dim tbJuegoSeleccionado As TextBlock = pagina.FindName("tbJuegoSeleccionado")
+        tbJuegoSeleccionado.Text = juego.Titulo
+
+        Dim gridAñadirTile As Grid = pagina.FindName("gridAñadirTile")
+        Interfaz.Pestañas.Visibilidad(gridAñadirTile, juego.Titulo, Nothing)
+
+        '---------------------------------------------
+
+        ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("animacionJuego", botonJuego)
+        Dim animacion As ConnectedAnimation = ConnectedAnimationService.GetForCurrentView().GetAnimation("animacionJuego")
+
+        If Not animacion Is Nothing Then
+            animacion.Configuration = New BasicConnectedAnimationConfiguration
+            animacion.TryStart(gridAñadirTile)
+        End If
+
+        '---------------------------------------------
+
+        Dim tbImagenTituloTextoTileAncha As TextBox = pagina.FindName("tbImagenTituloTextoTileAncha")
+        tbImagenTituloTextoTileAncha.Text = juego.Titulo
+        tbImagenTituloTextoTileAncha.Tag = juego.Titulo
+
+        Dim tbImagenTituloTextoTileGrande As TextBox = pagina.FindName("tbImagenTituloTextoTileGrande")
+        tbImagenTituloTextoTileGrande.Text = juego.Titulo
+        tbImagenTituloTextoTileGrande.Tag = juego.Titulo
+
+        '---------------------------------------------
+
+        Dim imagenPequeña As ImageEx = pagina.FindName("imagenTilePequeña")
+        imagenPequeña.Source = Nothing
+
+        Dim imagenMediana As ImageEx = pagina.FindName("imagenTileMediana")
+        imagenMediana.Source = Nothing
+
+        Dim imagenAncha As ImageEx = pagina.FindName("imagenTileAncha")
+        imagenAncha.Source = Nothing
+
+        Dim imagenGrande As ImageEx = pagina.FindName("imagenTileGrande")
+        imagenGrande.Source = Nothing
+
+        If Not juego.ImagenPequeña = Nothing Then
+            imagenPequeña.Source = juego.ImagenPequeña
+            imagenPequeña.Tag = juego.ImagenPequeña
+        End If
+
+        If Not juego.ImagenAncha = Nothing Then
+            If Not juego.ImagenMediana = Nothing Then
+                imagenMediana.Source = juego.ImagenMediana
+                imagenMediana.Tag = juego.ImagenMediana
+            Else
+                imagenMediana.Source = juego.ImagenAncha
+                imagenMediana.Tag = juego.ImagenAncha
+            End If
+
+            imagenAncha.Source = juego.ImagenAncha
+            imagenAncha.Tag = juego.ImagenAncha
+        End If
+
+        If Not juego.ImagenGrande = Nothing Then
+            imagenGrande.Source = juego.ImagenGrande
+            imagenGrande.Tag = juego.ImagenGrande
+        End If
+
+    End Sub
+
+End Module
